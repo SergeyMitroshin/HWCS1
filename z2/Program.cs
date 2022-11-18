e6432ef2d1ac3bcfeb1edeecfe640d4e4f556931
SergeyMitroshin <sergey@host-best.com>
2022-11-19 00:25:18 +0300

    v06

--- z2/Program.cs
+++ z2/Program.cs
@@ -1,5 +1,4 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
@@ -7,8 +6,7 @@
 Console.WriteLine("введите первое число");
 int num1 = int.Parse (Console.ReadLine());
 Console.WriteLine("введите второе число");
-//int num2 = int.Parse(Console.ReadLine());
-int num2 = 3;
+int num2 = int.Parse (Console.ReadLine());
 if  (num1 > num2)
 {
     Console.WriteLine("max = " + num1);
@@ -19,7 +17,3 @@ else
 }
 
 
-
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
--- z4/Program.cs
+++ z4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("введите первое число");
+int num1 = int.Parse (Console.ReadLine());
+Console.WriteLine("введите второе число");
+int num2 = int.Parse (Console.ReadLine());
+Console.WriteLine("введите третье число");
+int num3 = int.Parse (Console.ReadLine());
+int maxv = 0;
+if  (num1 > num2)  maxv = num1; else maxv = num2;
+if  (num3 > maxv)  maxv = num3;
+Console.WriteLine ("-> " + maxv);
