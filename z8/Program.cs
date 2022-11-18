a945403c30774a084579d16dc8986ec99ee75e4a
SergeyMitroshin <sergey@host-best.com>
2022-11-19 01:02:47 +0300

    v08

--- z8/Program.cs
+++ z8/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+string st = "";
+Console.WriteLine("Введите число");
+int num1 = int.Parse (Console.ReadLine());
+for (int i = 2; i <= num1; i+=2)
+{
+    st = st + i + ", ";
+}
+st = st.Substring(0, st.Length - 2); //убираем запятую и пробел в конце
+Console.WriteLine(st);
