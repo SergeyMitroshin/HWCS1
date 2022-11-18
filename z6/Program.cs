38d2afa5c5b5102418629e788916f2a73db63bf4
SergeyMitroshin <sergey@host-best.com>
2022-11-19 00:35:52 +0300

    v07

--- z6/Program.cs
+++ z6/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число");
+int num1 = int.Parse (Console.ReadLine());
+ int ostatok = num1 % 2;
+if (ostatok == 0) Console.WriteLine("да"); else Console.WriteLine("нет");
+
+
